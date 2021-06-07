e35f0f5d08948e48cac3ecebdfeb307f826002ca
Renan Gonçalves <renan.saddam@gmail.com>
2021-06-07 13:59:47 +0200

    Provide certificate and private key files for authentication/authorization

--- proxy.go
+++ proxy.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"crypto/tls"
+	"crypto/x509"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
@@ -13,12 +15,18 @@ import (
 
 var host string
 var port int
+
 var insecure bool
+var caFile, certFile, keyFile string
 
 func init() {
 	flag.StringVar(&host, "host", "", "Host to start the proxy (default all interfaces)")
 	flag.IntVar(&port, "port", 8080, "Port to start the proxy")
+
 	flag.BoolVar(&insecure, "insecure", false, "Allow insecure server connections when using SSL")
+	flag.StringVar(&caFile, "cacert", "", "CA certificate to verify peer against")
+	flag.StringVar(&certFile, "cert", "", "Client certificate file")
+	flag.StringVar(&keyFile, "key", "", "Private key file name")
 }
 
 func usage() {
@@ -40,6 +48,33 @@ func main() {
 		os.Exit(1)
 	}
 
+	tlsConfig := &tls.Config{
+		InsecureSkipVerify: insecure,
+	}
+
+	if caFile != "" {
+		caCert, err := ioutil.ReadFile(caFile)
+		if err != nil {
+			log.Print("Error loading certificate and/or private key file")
+			log.Print(err)
+			os.Exit(1)
+		}
+
+		tlsConfig.RootCAs = x509.NewCertPool()
+		tlsConfig.RootCAs.AppendCertsFromPEM(caCert)
+	}
+
+	if certFile != "" || keyFile != "" {
+		cert, err := tls.LoadX509KeyPair(certFile, keyFile)
+		if err != nil {
+			log.Print("Error loading certificate and/or private key file")
+			log.Print(err)
+			os.Exit(1)
+		}
+
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	}
+
 	address := fmt.Sprintf("%s:%d", host, port)
 
 	log.Printf("Proxying requests from %s to %s", address, target)
@@ -49,7 +84,7 @@ func main() {
 			r.Host = targetUri.Host
 		},
 		Transport: &http.Transport{
-			TLSClientConfig: &tls.Config{InsecureSkipVerify: insecure},
+			TLSClientConfig: tlsConfig,
 		},
 	}))
 }
